71c2b834c6c7fcedeecf66ea524c118f6ae8ebc2
TiraUndercoder <128243843+tiraUnderCode@users.noreply.github.com>
2023-06-10 06:01:48 +0000

    Delete src directory
    Signed-off-by: TiraUndercoder <128243843+tiraUnderCode@users.noreply.github.com>

--- src/index.ts
+++
@@ -1,6 +0,0 @@
-import { connectToWhatsApp } from "./whatsapp";
-import assert from "assert"
-
-assert(process.env.OPENAI_API_KEY, "OPENAI_API_KEY is required to run the app.")
-
-connectToWhatsApp()
